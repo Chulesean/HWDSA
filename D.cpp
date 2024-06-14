1f13767dd6aa322ea67aa7459da4c4381e1d31c8
Chulesean <151228599+Chulesean@users.noreply.github.com>
2024-06-14 15:55:26 +0300

    TASKDD

--- D.cpp
+++ D.cpp
@@ -1,65 +1,87 @@
 #include <climits>
+#include <deque>
 #include <iostream>
-#include <queue>
 #include <string>
 
-using namespace std;
+class MagicHat {
+ private:
+  std::deque<int> queue;
+  std::deque<int> minQueue;
+
+ public:
+  std::string enqueue(int n) {
+    queue.push_back(n);
+    while (!minQueue.empty() && minQueue.back() > n) {
+      minQueue.pop_back();
+    }
+    minQueue.push_back(n);
+    return "ok";
+  }
+
+  std::string dequeue() {
+    if (queue.empty()) {
+      return "error";
+    }
+    int removed = queue.front();
+    queue.pop_front();
+    if (removed == minQueue.front()) {
+      minQueue.pop_front();
+    }
+    return std::to_string(removed);
+  }
+
+  std::string front() {
+    if (queue.empty()) {
+      return "error";
+    }
+    return std::to_string(queue.front());
+  }
+
+  std::string size() { return std::to_string(queue.size()); }
+
+  std::string clear() {
+    queue.clear();
+    minQueue.clear();
+    return "ok";
+  }
+
+  std::string min() {
+    if (minQueue.empty()) {
+      return "error";
+    }
+    return std::to_string(minQueue.front());
+  }
+};
 
 int main() {
   int M;
-  cin >> M;
+  std::cin >> M;
+  std::cin.ignore();
 
-  queue<int> intelligenceQueue;
-  int minIntelligence = INT_MAX;
+  MagicHat hat;
+  std::string command;
+  std::deque<std::string> results;
 
-  while (M--) {
-    string command;
-    cin >> command;
-
-    if (command == "enqueue") {
-      int n;
-      cin >> n;
-      intelligenceQueue.push(n);
-      cout << "ok" << endl;
-      minIntelligence = min(minIntelligence, n);
+  for (int i = 0; i < M; ++i) {
+    std::getline(std::cin, command);
+    if (command.substr(0, 7) == "enqueue") {
+      int n = std::stoi(command.substr(8));
+      results.push_back(hat.enqueue(n));
     } else if (command == "dequeue") {
-      if (!intelligenceQueue.empty()) {
-        cout << intelligenceQueue.front() << endl;
-        if (intelligenceQueue.front() == minIntelligence) {
-          intelligenceQueue.pop();
-          minIntelligence = INT_MAX;
-          queue<int> tempQueue = intelligenceQueue;
-          while (!tempQueue.empty()) {
-            minIntelligence = min(minIntelligence, tempQueue.front());
-            tempQueue.pop();
-          }
-        } else {
-          intelligenceQueue.pop();
-        }
-      } else {
-        cout << "error" << endl;
-      }
+      results.push_back(hat.dequeue());
     } else if (command == "front") {
-      if (!intelligenceQueue.empty()) {
-        cout << intelligenceQueue.front() << endl;
-      } else {
-        cout << "error" << endl;
-      }
+      results.push_back(hat.front());
     } else if (command == "size") {
-      cout << intelligenceQueue.size() << endl;
+      results.push_back(hat.size());
     } else if (command == "clear") {
-      while (!intelligenceQueue.empty()) {
-        intelligenceQueue.pop();
-      }
-      cout << "ok" << endl;
-      minIntelligence = INT_MAX;
+      results.push_back(hat.clear());
     } else if (command == "min") {
-      if (!intelligenceQueue.empty()) {
-        cout << minIntelligence << endl;
-      } else {
-        cout << "error" << endl;
-      }
+      results.push_back(hat.min());
     }
+  }
+
+  for (const auto& result : results) {
+    std::cout << result << std::endl;
   }
 
   return 0;
